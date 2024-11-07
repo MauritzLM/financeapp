fd8499559a3d29231ee7c22951a9add3d0272aaf
Mauritz Marais <mauritz303@gmail.com>
2024-11-07 13:37:43 +0200

    included serializers for pot,budget and transaction

--- personalfinance/serializers.py
+++ personalfinance/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+from .models import Budget, Pot, Transaction
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,21 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ['avatar', 'name', 'category', 'date', 'amount', 'recurring']
+
+
+class BudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Budget
+        fields = ['category', 'maximum', 'theme']
+
+
+class PotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pot
+        fields = ['name', 'target', 'total', 'theme']        
