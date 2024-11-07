557803eac10a096763ace58b30e134266f1f5369
Mauritz Marais <mauritz303@gmail.com>
2024-11-07 13:39:27 +0200

    added personalfinance urls

--- api/urls.py
+++ api/urls.py
@@ -27,6 +27,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('finance-api/', include('personalfinance.urls')),
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
