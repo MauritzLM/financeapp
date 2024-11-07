6573080e7e90e358ffe6dc14ccc7eaca856df97a
Mauritz Marais <mauritz303@gmail.com>
2024-11-07 13:37:05 +0200

    added pot/budget url

--- personalfinance/urls.py
+++ personalfinance/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from . import views
+from .views import BudgetListView, IndexView, PotListView, BudgetDetailView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', IndexView.as_view()),
+    path('budget', BudgetListView.as_view()),
+    path('pot', PotListView.as_view()),
+    path('budget/<int:budget_id>/', BudgetDetailView.as_view()),
 ]
