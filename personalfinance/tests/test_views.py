7e4b0bfd0798c658c7b997a0816ecd5ced851c10
Mauritz Marais <mauritz303@gmail.com>
2024-11-09 09:38:52 +0200

    updated budget list test, added valid data test

--- personalfinance/tests/test_views.py
+++ personalfinance/tests/test_views.py
@@ -8,8 +8,8 @@ User = get_user_model()
 # index view test
 class IndexViewTest(TestCase):
     def setUp(self):
-         test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
-         test_user1.save()
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user1.save()
 
     def test_url_exists(self):
         client = APIClient()
@@ -22,47 +22,58 @@ class IndexViewTest(TestCase):
 # budget list view
 class BudgetListViewTest(TestCase):
      
-     def setUp(self):
-         test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
-         test_user1.save()
+    def setUp(self):
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user1.save()
 
-         Budget.objects.create(category='Entertainment', maximum=400.00, theme='#277C78', user=test_user1)
-         Budget.objects.create(category='Personal care', maximum=200.00, theme='#626070', user=test_user1)
+        Budget.objects.create(category='Entertainment', maximum=400.00, theme='#277C78', user=test_user1)
+        Budget.objects.create(category='Personal care', maximum=200.00, theme='#626070', user=test_user1)
      
-     def test_user_not_logged_in(self):
-         client = APIClient()
-         response = client.get('/finance-api/budget')
-         self.assertEqual(response.status_code, 403)
+    def test_user_not_logged_in(self):
+        client = APIClient()
+        response = client.get('/finance-api/budget')
+        self.assertEqual(response.status_code, 403)
 
     # get - all budgets of user then serialize then response = serialized data
-     def test_get_response(self):
-         client = APIClient()
+    def test_get_response(self):
+        client = APIClient()
         # login user
-         login = client.login(username='testuser1', password='1X<ISRUkw+tuK')
-         response = client.get('/finance-api/budget')
+        client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = client.get('/finance-api/budget')
 
-         self.assertEqual(response.status_code, 200)
-         self.assertEqual(len(response.data), 2)
-
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
 
 
     # post - post data then serialize data then save if valid
-     def test_post_with_valid_data(self):
-         client = APIClient()
-         login = client.login(username='testuser1', password='1X<ISRUkw+tuK')
+    def test_post_with_invalid_data(self):
+        client = APIClient()
+        client.login(username='testuser1', password='1X<ISRUkw+tuK')
 
-         data = {
+        data = {
              'category': 'Entertainment', 
              'maximum': 900.56,
-             'theme': '', 
-             'user': '',
+             'theme': '',
          }
 
-         response = client.post('/finance-api/budget', data)
-         print(response.data)
-         # add check for error* 
-         self.assertEqual(response.status_code, 400)
-         
+        response = client.post('/finance-api/budget', data)
+        # print(response.data)
+        # add check for error* 
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_with_valid_data(self):
+        client = APIClient()
+        client.login(username='testuser1', password='1X<ISRUkw+tuK')
+
+        data = {
+             'category': 'Entertainment', 
+             'maximum': 900.56,
+             'theme': '#626070',
+         }
+
+        response = client.post('/finance-api/budget', data)
+
+        self.assertEqual(response.status_code, 201)             
          
              
 
