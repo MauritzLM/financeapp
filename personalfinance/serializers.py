260357586710f2c3e5f6e96f5035eab2993a9225
Mauritz Marais <mauritz303@gmail.com>
2024-11-09 09:37:59 +0200

    updated user serializer, added write only extra_kwargs

--- personalfinance/serializers.py
+++ personalfinance/serializers.py
@@ -3,31 +3,41 @@ from rest_framework import serializers
 from .models import Budget, Pot, Transaction
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
-
-
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = ['avatar', 'name', 'category', 'date', 'amount', 'recurring']
+        fields = '__all__'
+        extra_kwargs = {'user': {'write_only': True}}
 
 
 class BudgetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Budget
-        fields = ['category', 'maximum', 'theme']
+        fields = '__all__'
+        extra_kwargs = {'user': {'write_only': True}}
 
 
 class PotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pot
-        fields = ['name', 'target', 'total', 'theme']        
+        fields = '__all__'
+        extra_kwargs = {'user': {'write_only': True}}
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user    
