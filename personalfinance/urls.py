f84e764fbfab487441d870cb3a1b659a1bab019b
Mauritz Marais <mauritz303@gmail.com>
2024-11-11 16:33:04 +0200

    added budget/category url

--- personalfinance/urls.py
+++ personalfinance/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import BudgetListView, IndexView, PotListView, BudgetDetailView
+from .views import BudgetListView, IndexView, PotListView, BudgetDetailView, BudgetSpendingView
+from .auth_views import UserCreate
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('overview', IndexView.as_view()),
+    path('users', UserCreate.as_view()),
     path('budget', BudgetListView.as_view()),
     path('pot', PotListView.as_view()),
-    path('budget/<int:budget_id>/', BudgetDetailView.as_view()),
+    path('budget/<int:budget_id>', BudgetDetailView.as_view()),
+    path('budget/<str:category>', BudgetSpendingView.as_view()),
 ]
