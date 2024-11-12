efc74535b538dec9b63bb65468c0b2b2728fd1e1
Mauritz Marais <mauritz303@gmail.com>
2024-11-12 16:32:28 +0200

    added transaction search and category urls

--- personalfinance/urls.py
+++ personalfinance/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import BudgetListView, IndexView, PotListView, BudgetDetailView, BudgetSpendingView
+from .views import BudgetListView, IndexView, PotListView, BudgetDetailView, BudgetSpendingView, TransactionListView, TransactionCategoryView, TransactionSearchView
 from .auth_views import UserCreate
 
 urlpatterns = [
@@ -10,4 +10,7 @@ urlpatterns = [
     path('pot', PotListView.as_view()),
     path('budget/<int:budget_id>', BudgetDetailView.as_view()),
     path('budget/<str:category>', BudgetSpendingView.as_view()),
+    path('transactions/<str:sort_by>/<int:page>', TransactionListView.as_view()),
+    path('transactions/category/<str:category>/<str:sort_by>/<int:page>', TransactionCategoryView.as_view()),
+    path('transactions/search/<str:search_term>/<str:sort_by>/<int:page>', TransactionSearchView.as_view()),
 ]
