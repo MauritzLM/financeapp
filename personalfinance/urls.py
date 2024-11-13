40b97e087909438a414977c02e13c31eb04131a8
Mauritz Marais <mauritz303@gmail.com>
2024-11-13 13:57:21 +0200

    added pot details url

--- personalfinance/urls.py
+++ personalfinance/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 
-from .views import BudgetListView, IndexView, PotListView, BudgetDetailView, BudgetSpendingView, TransactionListView, TransactionCategoryView, TransactionSearchView
+from .views import BudgetListView, IndexView, PotListView, BudgetDetailView, BudgetSpendingView, TransactionListView, TransactionCategoryView, TransactionSearchView, PotDetailView
 from .auth_views import UserCreate
 
 urlpatterns = [
     path('overview', IndexView.as_view()),
     path('users', UserCreate.as_view()),
-    path('budget', BudgetListView.as_view()),
-    path('pot', PotListView.as_view()),
-    path('budget/<int:budget_id>', BudgetDetailView.as_view()),
-    path('budget/<str:category>', BudgetSpendingView.as_view()),
+    path('budgets', BudgetListView.as_view()),
+    path('pots', PotListView.as_view()),
+    path('pots/<int:pot_id>', PotDetailView.as_view()),
+    path('budgets/<int:budget_id>', BudgetDetailView.as_view()),
+    path('budgets/<str:category>', BudgetSpendingView.as_view()),
     path('transactions/<str:sort_by>/<int:page>', TransactionListView.as_view()),
     path('transactions/category/<str:category>/<str:sort_by>/<int:page>', TransactionCategoryView.as_view()),
     path('transactions/search/<str:search_term>/<str:sort_by>/<int:page>', TransactionSearchView.as_view()),
