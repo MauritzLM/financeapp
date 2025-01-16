bedf563b7c043c68933330312109ff1bc4fb4cf1
Mauritz Marais <mauritz303@gmail.com>
2025-01-16 13:25:32 +0200

    added files and settings for deployment

--- api/settings.py
+++ api/settings.py
@@ -12,18 +12,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+from dotenv import load_dotenv
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)5$@4+q_(1zc3_onvf-ss+-*9z74c&3av@m3f9+g8z@ra1*rhp'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-)5$@4+q_(1zc3_onvf-ss+-*9z74c&3av@m3f9+g8z@ra1*rhp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['localhost']
 
@@ -90,6 +99,11 @@ DATABASES = {
     }
 }
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
